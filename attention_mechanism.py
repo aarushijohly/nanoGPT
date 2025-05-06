dc8339f5eed012d37eb041b360fa48dae2a71d3b
Aarushi Johly <84744344+aarushijohly@users.noreply.github.com>
2025-05-06 20:09:58 +0530

    Update attention_mechanism.py

---
+++ attention_mechanism.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+
+
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attention_scores = queries @ keys.T
+        attention_weights = torch.softmax(attention_scores / keys.shape**0.5, dim=-1)
+        context_vector = attention_weights @ values
+        return context_vector
+
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attention_scores = queries @ keys.T
+        attention_weights = torch.softmax(attention_scores / keys.shape**0.5, dim=-1)
+        context_vector = attention_weights @ values
+        return context_vector
+
+
+class CausalAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length)), diagonal=1)
+        # buffers are automatically moved to the appropriate device(CPU or GPU) along with our model, which will be relevant
+        # when training our LLM. This means we don't need to maually ensure these tensors are on the same device as your model parameters,
+        # avoiding the mismatch error.
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attention_score = queries @ keys.transpose(1,2)
+        attention_score.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attention_weights = torch.softmax(attention_score/keys.shape**0.5, dim=-1)
+        attention_weights = self.dropout(attention_weights)
+        context_vector = attention_weights @ values
+        return context_vector
+    
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList([
+            CausalAttention(d_in, d_out, context_length, dropout, qkv_bias)
+            for _ in range(num_heads)
+        ])
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+        "d_out must be divisible by num_heads"
+
+        self.d_out = 
+        self.num_heads = 
+
+
+
+
+
+
+
+
+
+
+
+
