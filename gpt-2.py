165f0abe1f1056027fe4abd49e2f1033a283ed5f
Aarushi Johly <84744344+aarushijohly@users.noreply.github.com>
2025-05-07 13:35:27 +0530

    Update gpt-2.py

--- gpt-2.py
+++ gpt-2.py
@@ -1,39 +1,9 @@
 import torch
 import torch.nn as nn
+#import config
 from attention_mechanism import MultiHeadAttention
+ 
 
-class GPTModel(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
-        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
-        self.trf_blocks = nn.Sequential(
-            *[DummyTransformerBlock(cfg)
-            for _ in range(cfg["n_layers"])]
-        )
-        self.final_norm = LayerNorm(cfg["emb_dim"])
-        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
-
-    def forward(self, x):
-        batch_size, seq_len = in_idx.shape
-        tok_embeds = self.tok_emb(in_idx)
-        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
-        x = tok_embeds + pos_embeds
-        x = self.drop_emb(x)
-        x = self.trf_blocks(x)
-        x = self.final_norm(x)
-        logits = self.out_head(x)
-        return logits
-    
-
-class DummyTransformerBlock(nn.Module):
-    def __init__(self, cfg):
-        super().__init__()
-
-    def forward(self, x):
-        return x
-    
 
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
@@ -75,7 +45,56 @@ class FeedForward(nn.Module):
 
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
-        pass
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            dropout=cfg["drop_rate"],
+            num_heads=cfg["n_heads"],
+            qkv_bias=False
+        )
+        self.ff=FeedForward(cfg)
+        self.norm1=LayerNorm(cfg["emb_dim"])
+        self.norm2=LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut=nn.Dropout(cfg["drop_rate"])
 
     def forward(self, x):
-        pass
+        shortcut=x
+        x=self.norm1(x)
+        x=self.att(x)
+        x=self.drop_shortcut(x)
+        x=x+shortcut
+
+        shortcut=x
+        x=self.norm2(x)
+        x=self.ff(x)
+        self.drop_shortcut(x)
+        x=x+shortcut
+        
+        return x
+    
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg)
+            for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
