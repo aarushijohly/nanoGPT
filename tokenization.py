e861d8bac0eeb95eb1573be511bdd918ce054cf2
Aarushi Johly <84744344+aarushijohly@users.noreply.github.com>
2025-05-09 20:02:26 +0530

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -1,14 +1,44 @@
 import os
 import re
 
-
-#implementing a simple text tokenizer
-
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab
-        self.int_to_str = {j:i for i, j in vocab.items()}
-
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
     def encode(self, text):
-        preprecessed = re.split(r'[,.?_!"()\']|--|\\s', text)
-        preprecessed = [item.strip() for item in     ]
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+    
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
